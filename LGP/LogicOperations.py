683484abd358b31062f3d4c43f14550a8aaed587
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2019-10-30 00:23:36 -0400

    add logic operations

---
+++ LGP/LogicOperations.py
@@ -0,0 +1,27 @@
+# This py file is part of LGP package, which consists of all avaliable logic opeartions as class.
+# 
+# 
+
+class AND:
+    def __init__(self, o1, o2):
+        self.oprand1 = o1
+        self.oprand2 = o2
+    
+    def equals(self):
+        return self.oprand1[0] and self.oprand2[0]
+
+class OR:
+    def __init__(self, o1, o2):
+        self.oprand1 = o1
+        self.oprand2 = o2
+    
+    def equals(self):
+        return self.oprand1[0] or self.oprand2[0]
+
+class XOR:
+    def __init__(self, o1, o2):
+        self.oprand1 = o1
+        self.oprand2 = o2
+    
+    def equals(self):
+        return self.oprand1[0] ^ self.oprand2[0]
