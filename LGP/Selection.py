be8ea0f700bc610797472c759fe90f3a44be04f5
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2020-02-03 12:47:45 -0500

    Add multiprocessing feature to tournament selection

--- LGP/Selection.py
+++ LGP/Selection.py
@@ -2,8 +2,9 @@
 import Evaluation
 import Individual
 import random
+from multiprocessing import Pool
 
-def tournament(population, tournamentSize, fitnessType, creator, data, label):
+def tournament(population, tournamentSize, fitnessType, creator, data, label, para=True):
     '''
     This function returns the index of the tournament winner and losser.
     Parameters:
@@ -19,5 +20,25 @@ def tournament(population, tournamentSize, fitnessType, creator, data, label):
     random.shuffle(l)
     pop_tour_index = l[:tournamentSize]
     # evaluate fitness
-    fitness_tour = [Evaluation.fitness(type=fitnessType,creator=creator,program=population[i], data = data, label = label) for i in pop_tour_index]
+    fitness_tour = []
+    if para == True:
+        fitness_tour = evaluateFitness_para(fitnessType,creator,population,pop_tour_index,data,label)
+    else:
+        fitness_tour = [Evaluation.fitness(type=fitnessType,creator=creator,program=population[i], data = data, label = label) for i in pop_tour_index]
     return pop_tour_index[fitness_tour.index(max(fitness_tour))], pop_tour_index[fitness_tour.index(min(fitness_tour))]
+
+# ---- Below: evaluate program with multiprocessing ----
+def evaluateFitness_para(ft,ct,pg,pgi,dt,lb):
+    inputParameter = [[ft,ct,pg[i],dt,lb]for i in pgi]
+    try:
+        pool = Pool(processes=16)
+        result = pool.map(evaluateFitness_para_mid,inputParameter)
+    finally:
+        pool.close()
+        pool.join()
+    return result
+
+def evaluateFitness_para_mid(parameters):
+    return Evaluation.fitness(type=parameters[0],creator=parameters[1],program=parameters[2], data = parameters[3], label = parameters[4])
+
+# ---- Above: evaluate program with multiprocessing ----
